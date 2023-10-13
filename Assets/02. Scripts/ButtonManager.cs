f7361b03d3c656413de783af8d92dd11cce14b3a
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2023-10-13 23:46:50 +0900

    ButtonAsset

---
+++ Assets/02. Scripts/ButtonManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        
+    }
+
+    public void StartBtn()
+    {
+
+    }
+
+    public void BGMBtn()
+    {
+
+    }
+
+    public void VFXBtn()
+    {
+
+    }
+}
--- Assets/02. Scripts/GameManager.cs
+++ Assets/02. Scripts/GameManager.cs
@@ -5,10 +5,34 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private bool isStart;
+    [SerializeField] private bool isStart = false;
     [SerializeField] private Canvas _inGameCanvas;
     [SerializeField] private GameObject _ingameObj;
     [SerializeField] private Canvas _lobbyCanvas;
 
+    private void Awake()
+    {
 
+    }
+
+    private void Update()
+    {
+        GameStart();
+    }
+
+    private void GameStart()
+    {
+        if (isStart == true)
+        {
+            _ingameObj.SetActive(true);
+            _inGameCanvas.enabled = true;
+            _lobbyCanvas.enabled = false;
+        }
+        else
+        {
+            _ingameObj.SetActive(false);
+            _inGameCanvas.enabled = false;
+            _lobbyCanvas.enabled = true;
+        }
+    }
 }
